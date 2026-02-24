229328c5047af59be59727122fc88174aa85363f
Alfin Risson <alfinrisson@gmail.com>
2026-02-24 15:35:25 +0100

    added feature to store data in local storage

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 let myInput = document.getElementById("taskInput");
-let btn = document.getElementById("addBtn");
+let addBtn = document.getElementById("addBtn");
 let list = document.getElementById("tasklist");
-let save = document.getElementById("saveBtn");
 
-btn.addEventListener("click", function(){
+let tasks = [];
+
+addBtn.addEventListener("click", function(){
     const taskText = myInput.value;
 
-    if(taskText === "") return;
+    if (taskText === "") return;
 
+    tasks.push(taskText)
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+    addTextToDOM(taskText);
+    myInput.value = "";
+
+   
+})
+
+function addTextToDOM(taskText){
     const li = document.createElement("li");
     li.textContent = taskText;
-    
+
     const dbtn = document.createElement("button");
     dbtn.textContent = "delete";
 
     dbtn.addEventListener("click", function(){
+        tasks = tasks.filter(task => task !== taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
         li.remove();
         dbtn.remove();
-       
     })
 
+     list.appendChild(li);
+     list.appendChild(dbtn);
+}
 
-
-    list.appendChild(li);
-    list.appendChild(dbtn);
+window.onload = function(){
+    const storedText = this.localStorage.getItem("tasks");
     
-    myInput.value = "";
-});
+    if(storedText){
+        tasks = JSON.parse(storedText);
+     tasks.forEach(task => addTextToDOM(task));
+    }
+}
 
-save.addEventListener("click", function(){
-    save.textContent = "yes";
-})
